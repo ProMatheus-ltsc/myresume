a7109b24550b4f0ee4fccafd64091efed8a23687
李云飞 <484471679@qq.com>
2022-05-22 17:51:47 +0000

    resume

--- run.sh
+++ run.sh
@@ -6,3 +6,5 @@ git remote add orgin git@github.com:ProMatheus-ltsc/myresume.git
 git branch -M master
 git push -u orgin master
 
+git remote add origin https://gitee.com/Promatheus-lstc/resume.git
+git push -f origin master
